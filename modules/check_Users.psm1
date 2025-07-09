c99139b1d1ec1ae97dc22bfe10f1e71e4766d78a
zh54321 <donotspamme@tutanota.com>
2025-07-09 06:31:19 +0200

    Improved status messages

--- modules/check_Users.psm1
+++ modules/check_Users.psm1
@@ -143,6 +143,7 @@ function Invoke-CheckUsers {
 
 
     # Checking if users SignInActivity property can be retrieved. Requires Premium otherwise HTTP 403:Tenant is not a B2C tenant and doesn't have premium license
+    write-host "[*] Check if SignInActivity can be retrived"
     $QueryParameters = @{
         '$select' = "id,SignInActivity"
         '$top' = "1"
